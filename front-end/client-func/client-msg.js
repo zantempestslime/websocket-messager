c25579fd965539319bb101111c9cb659a8367313
MapleYaki <zantempestslime@gmail.com>
2026-07-29 23:36:32 +0800

    Add files via upload

---
+++ front-end/client-func/client-msg.js
@@ -0,0 +1,15 @@
+const ws = new WebSocket("wss://websocket-messager-1.onrender.com"); // Connected to my WebSocket Server
+
+ws.onopen = () => { // when connections established
+    ws.send(JSON.stringify({ type: 'register', userId: 'alice' }));
+};
+
+ws.onmessage = (event) => { // When server sends this user something...
+    const msg = JSON.parse(event.data); // parses the event data
+    console.log(`${msg.from}: ${msg.text}`); // logs the data
+}
+
+function sendMessage(to, text) { // a function used to send user message, like with send button.
+    ws.send(JSON.stringify({ type: 'relay', to, text }))
+}
+
